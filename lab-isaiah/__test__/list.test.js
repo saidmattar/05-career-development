9e31c37becae35899ecb27609f9d4c0f0a10a9b1
lonewalker72 <walkisai.72@gmail.com>
2017-08-28 08:55:49 -0700

    need resubmit

---
+++ lab-isaiah/__test__/list.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const List = require('../lib/list');
+
+describe('Testing List Module:', function() {
+  beforeAll(() => this.list = new List());
+  afterAll(() => delete this.list);
+
+  describe('Given a new instance it...', () => {
+
+    test('should have a length property of 0' , done => {
+      expect(this.list.length).toBe(0);
+      done();
+    });
+  });
+});
